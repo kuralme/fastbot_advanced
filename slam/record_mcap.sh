577fef2cfd660aa6b679cec5fef008266b75a357
kuralme <kuralme@protonmail.com>
2026-05-04 12:32:13 -0400

    slam: map publisher added

--- slam/record_mcap.sh
+++ slam/record_mcap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ros2 bag record -s mcap --storage-config-file recording_config.yaml  \
+ros2 bag record -s mcap --storage-config-file mcap_config.yaml  \
   /tf \
   /tf_static \
   /fastbot/camera_odom \
   /fastbot/keyframes \
   /fastbot/keyframes_2d \
+  /fastbot/map_points \
   /robot_description \
   /oak/left/image_rect/compressed \
   /oak/right/image_rect/compressed
